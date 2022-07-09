c5021f1c85b3d80c20d121b771bbfff1c3f86523
DovgaNik <nikita.dovgan.05@hotmail.com>
2022-07-09 21:22:54 +0300

    Done code to calculate average frequency.

--- freq.cpp
+++ freq.cpp
@@ -8,7 +8,10 @@ int main() {
 	string file, temp;
 	string delimiter = ":";
 	ifstream inputFileStream("/proc/cpuinfo");
-	
+
+	int count = 0, 
+	    sum = 0;
+
 	while (getline (inputFileStream, temp)) {
 		
 		file = file + "\n" + temp;
@@ -16,15 +19,18 @@ int main() {
 		string token = temp.substr(0, temp.find(delimiter));
 		if (token == "cpu MHz		"){
 			
-			cout << token << endl;
+			string Hz = temp.substr(11, 4);
+			int hz = stoi(Hz);
 
+			sum = sum + hz;
+			count++;
 		}
 
 	}
-	
-	inputFileStream.close(); 
 
-//	cout << file << endl;
+	cout << sum / count;	
+
+	inputFileStream.close(); 
 
 	return(0);
 
