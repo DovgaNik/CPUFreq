f4f92f4170b50862d83f859e356af9809bd343b7
DovgaNik <nikita.dovgan.05@hotmail.com>
2022-07-14 17:50:28 +0300

    Finished basic functionality of rust version. To time it outputs the number of cores and the sum of Hz in all the cores.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::io::{prelude::*, BufReader};
 
 const FILE_PATH: &str = "/proc/cpuinfo";
+const DELIMITER: char = ':';
 
 fn main() {
 
-    let average: (u16, u32); // Declaring a tuple to contain the number of cores and the sum of Hz
+	let mut average: (u16, f32) = (0, 0.0); // Declaring a tuple to contain the number of cores and the sum of Hz
+	
+	let file = File::open(FILE_PATH).unwrap();
+	let reader = BufReader::new(file);
 
-    let file = File::open(FILE_PATH).unwrap();
-    let reader = BufReader::new(file);
-
-    for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
+	for (_, line) in reader.lines().enumerate() {
+	
+	    let line = line.unwrap();
+        let split: Vec<&str> = line.split(DELIMITER).collect();
         
-        println!("{}", line);
-    }
+        if split[0] == "cpu MHz		"{
 
+            average.0 = average.0 + 1;
+            average.1 = average.1 + split[1].trim().parse::<f32>().unwrap();
 
+            let temp = split[1].trim().parse::<f32>().unwrap();
+
+        }
+	}
+
+	println!("{}", average.0);
+	println!("{}", average.1);
 }
