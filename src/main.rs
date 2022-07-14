2b847851863a92643f2cb92a3c417ba75a9d7fa4
DovgaNik <nikita.dovgan.05@hotmail.com>
2022-07-14 20:34:53 +0300

    Implemented almost as in C++.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
+use std::{thread, time};
 
 const FILE_PATH: &str = "/proc/cpuinfo";
 const DELIMITER: char = ':';
 
 fn main() {
 
-	let mut average: (u16, f32) = (0, 0.0); // Declaring a tuple to contain the number of cores and the sum of Hz
-	
-	let file = File::open(FILE_PATH).unwrap();
-	let reader = BufReader::new(file);
+    loop {
 
-	for (_, line) in reader.lines().enumerate() {
-	
-	    let line = line.unwrap();
+        get_freq();
+        thread::sleep(time::Duration::from_secs(2));
+
+    }
+
+}
+
+fn get_freq() {
+
+    let mut average: (u16, f32) = (0, 0.0); // Declaring a tuple to contain the number of cores and the sum of Hz.
+
+    let file = File::open(FILE_PATH).unwrap(); //|This blob of code opens the file with CPU 
+    let reader = BufReader::new(file);         //|information.
+
+    for (_, line) in reader.lines().enumerate() {
+
+        let line = line.unwrap();
         let split: Vec<&str> = line.split(DELIMITER).collect();
-        
+
         if split[0] == "cpu MHz		"{
 
-            average.0 = average.0 + 1;
-            average.1 = average.1 + split[1].trim().parse::<f32>().unwrap();
+            let freq = split[1].trim().parse::<f32>().unwrap();
+            
+            println!("Core {core} is {coreFreq} GHz", core = average.0, coreFreq = freq / 1000.0);
 
-            //let temp = split[1].trim().parse::<f32>().unwrap();
+            average.1 = average.1 + freq;
+            average.0 = average.0 + 1;
 
         }
-	}
 
-	println!("{}", average.0);
-	println!("{}", average.1);
+    }
+    
+    println!("The average frequency among the cores is {averageHz} GHz", averageHz = average.1 / average.0 as f32 / 1000.0);
+
 }
